fb33f4a358b5c9d5581b2b67688b4c71f8012338
akmhmgc <akimuhamaguchi@icloud.com>
2023-03-24 00:42:08 +0900

    デプロイ時にファイルのアップロードが失敗する問題を修正

--- main.py
+++ main.py
@@ -1,9 +1,26 @@
-import os
+import tempfile
+import base64
 from pathlib import Path
 from llama_index import download_loader
 import streamlit as st
 from langchain.chat_models import ChatOpenAI
 from llama_index import GPTSimpleVectorIndex, LLMPredictor, download_loader
+
+def show_pdf(file_path:str):
+    """Show the PDF in Streamlit
+    That returns as html component
+
+    Parameters
+    ----------
+    file_path : [str]
+        Uploaded PDF file path
+    """
+
+    with open(file_path, "rb") as f:
+        base64_pdf = base64.b64encode(f.read()).decode("utf-8")
+    pdf_display = f'<embed src="data:application/pdf;base64,{base64_pdf}" width="100%" height="1000" type="application/pdf">'
+    return pdf_display
+
 
 st.title("PDF Indexer")
 
@@ -20,13 +37,19 @@ if openai_api_key:
 
     if uploaded_file and st.button("Make index data"):
         with st.spinner("Loading data..."):
-            PDFReader = download_loader("PDFReader")
-            loader = PDFReader()
-            documents = loader.load_data(file=uploaded_file.name)
-            st.success("Data loaded successfully!")
+            with tempfile.NamedTemporaryFile(delete=False) as tmp_file:
+                fp = Path(tmp_file.name)
+                fp.write_bytes(uploaded_file.getvalue())
+                st.write(show_pdf(tmp_file.name))
 
-            llm_predictor = LLMPredictor(llm=ChatOpenAI(temperature=0, model_name="gpt-3.5-turbo", openai_api_key=openai_api_key))
-            st.session_state.index = GPTSimpleVectorIndex(documents, llm_predictor=llm_predictor)
+                PDFReader = download_loader("PDFReader")
+                loader = PDFReader()
+                documents = loader.load_data(file=fp)
+                st.success("Data loaded successfully!")
+                print(documents)
+
+                llm_predictor = LLMPredictor(llm=ChatOpenAI(temperature=0, model_name="gpt-3.5-turbo", openai_api_key=openai_api_key))
+                st.session_state.index = GPTSimpleVectorIndex(documents, llm_predictor=llm_predictor)
 
     if st.session_state.index:
         user_question = st.text_input("Enter your question:", value="")
